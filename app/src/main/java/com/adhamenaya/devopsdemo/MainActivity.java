3536f87dfd8ead2399b6fa2b1d836274f7b414b2
adhamenaya <adhamenaya@gmail.com>
2020-03-10 11:27:09 +0200

    Add image tio the main screen

--- app/src/main/java/com/adhamenaya/devopsdemo/MainActivity.java
+++ app/src/main/java/com/adhamenaya/devopsdemo/MainActivity.java
@@ -3,12 +3,17 @@ package com.adhamenaya.devopsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make change
     }
 }
